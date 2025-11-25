f6da30796d17a11198fea58ec3b894f6b0286e1e
Denis Kovalev <overshake777@mail.ru>
2025-11-26 01:51:46 +0400

    added create, insert and request to weeks table

--- dbInit.py
+++ dbInit.py
@@ -2,7 +2,7 @@ import sqlite3
 
 DB_NAME = "expenses.db"
 
-def init_db():
+def init_db_expenses():
     conn = sqlite3.connect(DB_NAME)
     cur = conn.cursor()
 
@@ -21,6 +21,22 @@ def init_db():
     conn.commit()
     conn.close()
 
+def init_db_weeks():
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+
+    cur.execute("""
+                CREATE TABLE IF NOT EXISTS weeks(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Year INTEGER NOT NULL,
+                    Week INTEGER NOT NULL,
+                    Date TEXT NOT NULL,
+                )
+                """)
+
+    conn.commit()
+    conn.close()
+
 def save_new_expense(amount, currency, date, time, place, fixed):
     conn = sqlite3.connect(DB_NAME)
     cur = conn.cursor()
@@ -28,7 +44,43 @@ def save_new_expense(amount, currency, date, time, place, fixed):
     cur.execute("""
         INSERT INTO expenses (amount, currency, date, time, place, fixed)
         VALUES (?, ?, ?, ?, ?, ?)
-    """, (amount, currency. date, time. place, fixed))
+    """, (amount, currency, date, time, place, fixed))
 
     conn.commit()
     conn.close()
+
+def save_new_weeks(year, week, date):
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+
+    cur.execute("""
+        INSERT INTO weeks (Year, Week, Date)
+        VALUES (?, ?, ?)
+    """, (year, week, date))
+
+    conn.commit()
+    conn.close()
+
+def db_last_date_request():
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+
+    cursor = cur.execute("""SELECT name FROM sqlite_master 
+                            WHERE type='table' AND name='weeks';""")
+
+    result = cursor.fetchone()
+
+    if result:
+        cursor.execute("SELECT date FROM WEEKS ORDER BY id DESC LIMIT 1")
+        last_row = cursor.fetchone()
+
+        if last_row:
+            conn.close()
+            return last_row[0]
+        else:
+            conn.close()
+            return None
+
+    else:
+        conn.close()
+        return None
