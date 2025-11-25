250972b14a9a1e1ba0482999b47ef19a100f7de7
Denis Kovalev <overshake777@mail.ru>
2025-11-25 19:06:20 +0400

    added call of db view function

--- mainwindow.py
+++ mainwindow.py
@@ -1,12 +1,10 @@
 # This Python file uses the following encoding: utf-8
+import dbInit
+import textParser
+
 import sys
 
 from PySide6.QtWidgets import QApplication, QMainWindow
-
-# Important:
-# You need to run the following command to generate the ui_form.py file
-#     pyside6-uic form.ui -o ui_form.py, or
-#     pyside2-uic form.ui -o ui_form.py
 from ui_form import Ui_MainWindow
 
 class MainWindow(QMainWindow):
@@ -14,6 +12,15 @@ class MainWindow(QMainWindow):
         super().__init__(parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        self.ui.addExpensesTextBtn.clicked.connect(self.addExpensesTextBtn_click)
+
+    def addExpensesTextBtn_click(self):
+        blocks = textParser.text_parser(self.ui.addExpensesTextEdit.toPlainText())
+        operations = textParser.blocks_parser(blocks)
+        self.ui.addExpensesTextEdit.clear()
+
+
 
 
 if __name__ == "__main__":
